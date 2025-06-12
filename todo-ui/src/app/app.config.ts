877c1a3a678610f996ae076b5e43f007ba4a0595
jones <banlonjones@urpharm.com>
2025-06-12 16:21:16 +0100

    refactor sign-in and sign-up components for improved layout and styling; update task component buttons for consistency

--- todo-ui/src/app/app.config.ts
+++ todo-ui/src/app/app.config.ts
@@ -11,11 +11,13 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import {FIREBASE_OPTIONS} from '@angular/fire/compat';
 import {environment} from '../environments/environment';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideAnimationsAsync(),
     providePrimeNG({
       theme: {
         preset: Lara
