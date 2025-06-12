8302f9da24a525e8e8cc527234807cb531fe7193
jones <banlonjones@urpharm.com>
2025-06-12 13:27:15 +0100

    refactor Firebase configuration to use environment variables and add TypeScript support

--- todo-api/src/configs/firebaseConfig.js
+++ todo-api/src/configs/firebaseConfig.js
@@ -1,11 +1,14 @@
 import admin from "firebase-admin";
-import * as path from "node:path";
-
-const serviceAccount = path.resolve("./todo-app-309e2-firebase-adminsdk-fbsvc-658419b9d5.json");
+import dotenv from "dotenv";
+dotenv.config();
 
 const fbApp = admin.initializeApp(
     {
-        credential: admin.credential.cert(serviceAccount),
+        credential: admin.credential.cert({
+            project_id: process.env.FIREBASE_PROJECT_ID,
+            private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+            client_email: process.env.FIREBASE_CLIENT_EMAIL,
+        }),
     }
 );
 export default fbApp;
